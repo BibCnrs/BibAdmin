1d96508d206c84cf2593ef178f679953a8d48357
ThieryMichel <thiery@marmelab.com>
2016-06-20 14:33:47 +0200

    add units/domain/instituts filter for janusAccount

--- js/config/janusAccounts.js
+++ js/config/janusAccounts.js
@@ -10,7 +10,7 @@ export default function (nga, admin) {
         .title('Comptes JANUS')
         .perPage(20)
         .fields([
-            nga.field('username').isDetailLink(true).label('Username'),
+            nga.field('janus_account.username').map((_, entry) => entry.username).isDetailLink(true).label('Username'),
             nga.field('domains', 'reference_many').targetEntity(domain).targetField(nga.field('name')).label('Domaines'),
             nga.field('all_domains', 'reference_many').targetEntity(domain).targetField(nga.field('name')).label('Tous les domaines'),
             nga.field('primary_institute', 'reference').targetEntity(institute).targetField(nga.field('name')).label('Institut principal'),
@@ -20,9 +20,42 @@ export default function (nga, admin) {
         ])
         .filters([
             nga.field('match').label('Recherche global').pinned(true),
-            nga.field('like_username').label('Login'),
-            nga.field('primary_institute', 'reference').targetEntity(institute).targetField(nga.field('name')).label('Institut principal'),
-            nga.field('primary_unit', 'reference').targetEntity(unit).targetField(nga.field('name')).label('Unité principale')
+            nga.field('like_janus_account.username').label('Login'),
+            nga.field('domain.name', 'reference')
+            .targetEntity(domain)
+            .targetField(nga.field('name'))
+            .remoteComplete(true)
+            .label('Domaines'),
+            nga.field('janus_account.primary_institute', 'reference')
+            .targetEntity(institute)
+            .targetField(nga.field('like_name').map((_, entry) => entry.name))
+            .remoteComplete(true)
+            .label('Institut principal'),
+            nga.field('janus_account.primary_unit', 'reference')
+            .targetEntity(unit)
+            .targetField(nga.field('like_name').map((_, entry) => entry.name))
+            .remoteComplete(true)
+            .label('Unité principale'),
+            nga.field('unit.id', 'reference')
+            .targetEntity(unit)
+            .targetField(nga.field('like_name').map((_, entry) => entry.name))
+            .remoteComplete(true)
+            .label('Unité(Nom)'),
+            nga.field('unit.id', 'reference')
+            .targetEntity(unit)
+            .targetField(nga.field('like_code').map((_, entry) => entry.code))
+            .remoteComplete(true)
+            .label('Unité(Code)'),
+            nga.field('institute.id', 'reference')
+            .targetEntity(institute)
+            .targetField(nga.field('like_name').map((_, entry) => entry.name))
+            .remoteComplete(true)
+            .label('Institut(Nom)'),
+            nga.field('institute.id', 'reference')
+            .targetEntity(institute)
+            .targetField(nga.field('like_code').map((_, entry) => entry.code))
+            .remoteComplete(true)
+            .label('Institut(Code)')
         ])
         .sortField('username')
         .sortDir('DESC')
