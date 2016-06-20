f1951bfabdc6b3a493445b318edb6231d1480c43
ThieryMichel <thiery@marmelab.com>
2016-06-20 12:04:55 +0200

    add units/domain/instituts filter for inistAccount

--- js/config/inistAccounts.js
+++ js/config/inistAccounts.js
@@ -10,27 +10,46 @@ export default function (nga, admin) {
         .title('Compte INIST')
         .perPage(20)
         .fields([
-            nga.field('username').isDetailLink(true).label('Login'),
-            nga.field('name').isDetailLink(true).label('Nom'),
-            nga.field('firstname').isDetailLink(true).label('Prénom'),
-            nga.field('mail').isDetailLink(true).label('courriel'),
-            nga.field('subscription_date', 'date').label('Date d\'inscription'),
-            nga.field('expiration_date', 'date').label('Date d\'expiration'),
+            nga.field('inist_account.username').map((_, entry) => entry.username).isDetailLink(true).label('Login'),
+            nga.field('inist_account.name').map((_, entry) => entry.name).isDetailLink(true).label('Nom'),
+            nga.field('inist_account.firstname').map((_, entry) => entry.firstname).isDetailLink(true).label('Prénom'),
+            nga.field('inist_account.mail').map((_, entry) => entry.mail).isDetailLink(true).label('courriel'),
+            nga.field('inist_account.subscription_date', 'date').map((_, entry) => entry.subscription_date).label('Date d\'inscription'),
+            nga.field('inist_account.expiration_date', 'date').map((_, entry) => entry.expiration_date).label('Date d\'expiration'),
             nga.field('domains', 'reference_many').targetEntity(domain).targetField(nga.field('name')).label('Domaines'),
             nga.field('all_domains', 'reference_many').targetEntity(domain).targetField(nga.field('name')).label('Tous les domaines'),
-            nga.field('institutes', 'reference_many').targetEntity(institute).targetField(nga.field('name')).label('Instituts secondaire'),
+            nga.field('institutes', 'reference_many').targetEntity(institute).targetField(nga.field('name')).label('Instituts'),
             nga.field('units', 'reference_many').targetEntity(unit).targetField(nga.field('name')).label('Unités')
         ])
         .filters([
             nga.field('match').label('Recherche global').pinned(true),
-            nga.field('like_username').label('Login'),
-            nga.field('like_name').label('Nom'),
-            nga.field('like_firstname').label('Prénom'),
-            nga.field('like_mail').label('courriel'),
-            nga.field('from_subscription_date', 'date').label('Date d\'inscription aprés'),
-            nga.field('to_subscription_date', 'date').label('Date d\'inscription avant'),
-            nga.field('from_expiration_date', 'date').label('Date d\'expiration aprés'),
-            nga.field('to_expiration_date', 'date').label('Date d\'expiration avant')
+            nga.field('inist_account.id', 'reference').targetEntity(inistAccount).targetField(nga.field('username')).remoteComplete(true).label('Login'),
+            nga.field('like_inist_account.name').label('Nom'),
+            nga.field('like_inist_account.firstname').label('Prénom'),
+            nga.field('like_inist_account.mail').label('courriel'),
+            nga.field('from_inist_account.subscription_date', 'date').label('Date d\'inscription aprés'),
+            nga.field('to_inist_account.subscription_date', 'date').label('Date d\'inscription avant'),
+            nga.field('from_inist_account.expiration_date', 'date').label('Date d\'expiration aprés'),
+            nga.field('to_inist_account.expiration_date', 'date').label('Date d\'expiration avant'),
+            nga.field('domain.name', 'reference').targetEntity(domain).targetField(nga.field('name')).label('Domaines'),
+            nga.field('institute.id', 'reference')
+            .targetEntity(institute)
+            .targetField(nga.field('like_name').map((_, entry) => entry.name))
+            .label('Instituts(Nom)'),
+            nga.field('institute.id', 'reference')
+            .targetEntity(institute)
+            .targetField(nga.field('like_code').map((_, entry) => entry.code))
+            .label('Instituts(Code)'),
+            nga.field('unit.id', 'reference')
+            .targetEntity(unit)
+            .targetField(nga.field('like_name').map((_, entry) => entry.name))
+            .remoteComplete(true)
+            .label('Unités(Nom)'),
+            nga.field('unit.id', 'reference')
+            .targetEntity(unit)
+            .targetField(nga.field('like_code').map((_, entry) => entry.code))
+            .remoteComplete(true)
+            .label('Unités(Code)')
         ])
         .exportFields([
             nga.field('username').label('Login'),
