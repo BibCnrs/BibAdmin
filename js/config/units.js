76fb15ae68197aea4f7b333304362ac0df80f5d1
ThieryMichel <thiery@marmelab.com>
2016-06-20 15:29:20 +0200

    add domain and institute filter for unit

--- js/config/units.js
+++ js/config/units.js
@@ -9,15 +9,25 @@ export default function (nga, admin) {
         .title('Unités')
         .perPage(20)
         .fields([
-            nga.field('code').isDetailLink(true),
-            nga.field('name').label('Nom'),
+            nga.field('unit.code').map((_, entry) => entry.code).isDetailLink(true),
+            nga.field('unit.name').map((_, entry) => entry.name).label('Nom'),
             nga.field('institutes', 'reference_many').targetEntity(institute).targetField(nga.field('name')).label('Intituts'),
-            nga.field('domains', 'reference_many').targetEntity(domain).targetField(nga.field('name')).label('Domaines')
+            nga.field('domains', 'reference_many').targetEntity(domain).targetField(nga.field('name')).label('Domaines'),
+            nga.field('all_domains', 'reference_many').targetEntity(domain).targetField(nga.field('name')).label('Domaines Hérités')
         ])
         .filters([
             nga.field('match').label('Recherche global').pinned(true),
-            nga.field('like_name').label('Nom'),
-            nga.field('like_code').label('Code')
+            nga.field('like_unit.name').label('Nom'),
+            nga.field('like_unit.code').label('Code'),
+            nga.field('domain.name', 'reference').targetEntity(domain).targetField(nga.field('name')).label('Domaines'),
+            nga.field('institute.id', 'reference')
+            .targetEntity(institute)
+            .targetField(nga.field('like_name').map((_, entry) => entry.name))
+            .label('Instituts(Nom)'),
+            nga.field('institute.id', 'reference')
+            .targetEntity(institute)
+            .targetField(nga.field('like_code').map((_, entry) => entry.code))
+            .label('Instituts(Code)')
         ])
         .sortField('name')
         .sortDir('DESC')
