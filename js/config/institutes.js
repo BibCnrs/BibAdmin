f0c0855423e0001fc5972528f0bf1a4e7cae0823
ThieryMichel <thiery@marmelab.com>
2016-08-23 17:09:21 +0200

    coding style

--- js/config/institutes.js
+++ js/config/institutes.js
@@ -4,30 +4,30 @@ export default function (nga, admin) {
     const domain = admin.getEntity('domains');
 
     institute.listView()
-        .actions(['export', 'filter', 'create', 'batch'])
-        .title('Instituts')
-        .perPage(20)
-        .fields([
-            nga.field('institute.code').map((_, entry) => entry.code).isDetailLink(true).label('Code'),
-            nga.field('institute.name').map((_, entry) => entry.name).label('Nom'),
-            nga.field('domains', 'reference_many').targetEntity(domain).targetField(nga.field('name')).label('Communautés')
-        ])
-        .filters([
-            nga.field('match').label('Recherche global').pinned(true),
-            nga.field('like_institute.name').label('Nom'),
-            nga.field('like_institute.code').label('Code'),
-            nga.field('domain.name', 'reference')
-            .targetEntity(domain)
-            .targetField(nga.field('name'))
-            .remoteComplete(true)
-            .label('Communautés')
-        ])
-        .sortField('name')
-        .sortDir('DESC')
-        .exportFields([
-            institute.listView().fields()
-        ])
-        .listActions(['edit', 'delete']);
+    .actions(['export', 'filter', 'create', 'batch'])
+    .title('Instituts')
+    .perPage(20)
+    .fields([
+        nga.field('institute.code').map((_, entry) => entry.code).isDetailLink(true).label('Code'),
+        nga.field('institute.name').map((_, entry) => entry.name).label('Nom'),
+        nga.field('domains', 'reference_many').targetEntity(domain).targetField(nga.field('name')).label('Communautés')
+    ])
+    .filters([
+        nga.field('match').label('Recherche global').pinned(true),
+        nga.field('like_institute.name').label('Nom'),
+        nga.field('like_institute.code').label('Code'),
+        nga.field('domain.name', 'reference')
+        .targetEntity(domain)
+        .targetField(nga.field('name'))
+        .remoteComplete(true)
+        .label('Communautés')
+    ])
+    .sortField('name')
+    .sortDir('DESC')
+    .exportFields([
+        institute.listView().fields()
+    ])
+    .listActions(['edit', 'delete']);
 
     institute.editionView()
     .title('Institut {{ entry.values.name }}')
@@ -43,7 +43,7 @@ export default function (nga, admin) {
         nga.field('code'),
         nga.field('name').label('Nom'),
         nga.field('domains', 'reference_many').targetEntity(admin.getEntity('domains')).targetField(nga.field('name')).label('Communautés')
-    ])
+    ]);
 
     return institute;
 }
--- js/config/janusAccounts.js
+++ js/config/janusAccounts.js
@@ -6,63 +6,64 @@ export default function (nga, admin) {
     const institute = admin.getEntity('institutes');
 
     janusAccount.listView()
-        .actions(['export', 'filter', 'batch'])
-        .title('Comptes JANUS')
-        .perPage(20)
-        .fields([
-            nga.field('janus_account.username').map((_, entry) => entry.username).isDetailLink(true).label('Username'),
-            nga.field('domains', 'reference_many').targetEntity(domain).targetField(nga.field('name')).label('Communautés'),
-            nga.field('all_domains', 'reference_many').targetEntity(domain).targetField(nga.field('name')).label('Communautés hérités'),
-            nga.field('primary_institute', 'reference').targetEntity(institute).targetField(nga.field('name')).label('Institut Janus'),
-            nga.field('additional_institutes', 'reference_many').targetEntity(institute).targetField(nga.field('name')).label('Instituts secondaire'),
-            nga.field('primary_unit', 'reference').targetEntity(unit).targetField(nga.field('name')).label('Unité Janus'),
-            nga.field('additional_units', 'reference_many').targetEntity(unit).targetField(nga.field('name')).label('Unités secondaires')
-        ])
-        .filters([
-            nga.field('match').label('Recherche global').pinned(true),
-            nga.field('like_janus_account.username').label('Login'),
-            nga.field('domain.name', 'reference')
-            .targetEntity(domain)
-            .targetField(nga.field('name'))
-            .remoteComplete(true)
-            .label('Communautés'),
-            nga.field('janus_account.primary_institute', 'reference')
-            .targetEntity(institute)
-            .targetField(nga.field('like_name').map((_, entry) => entry.name))
-            .remoteComplete(true)
-            .label('Institut Janus'),
-            nga.field('janus_account.primary_unit', 'reference')
-            .targetEntity(unit)
-            .targetField(nga.field('like_name').map((_, entry) => entry.name))
-            .remoteComplete(true)
-            .label('Unité Janus'),
-            nga.field('unit.id', 'reference')
-            .targetEntity(unit)
-            .targetField(nga.field('like_name').map((_, entry) => entry.name))
-            .remoteComplete(true)
-            .label('Unité(Nom)'),
-            nga.field('unit.id', 'reference')
-            .targetEntity(unit)
-            .targetField(nga.field('like_code').map((_, entry) => entry.code))
-            .remoteComplete(true)
-            .label('Unité(Code)'),
-            nga.field('institute.id', 'reference')
-            .targetEntity(institute)
-            .targetField(nga.field('like_name').map((_, entry) => entry.name))
-            .remoteComplete(true)
-            .label('Institut(Nom)'),
-            nga.field('institute.id', 'reference')
-            .targetEntity(institute)
-            .targetField(nga.field('like_code').map((_, entry) => entry.code))
-            .remoteComplete(true)
-            .label('Institut(Code)')
-        ])
-        .sortField('username')
-        .sortDir('DESC')
-        .exportFields([
-            janusAccount.listView().fields()
-        ])
-        .listActions(['edit', 'delete']);
+    .actions(['export', 'filter', 'batch'])
+    .title('Comptes JANUS')
+    .perPage(20)
+    .fields([
+        nga.field('janus_account.username').map((_, entry) => entry.username).isDetailLink(true).label('Username'),
+        nga.field('domains', 'reference_many').targetEntity(domain).targetField(nga.field('name')).label('Communautés'),
+        nga.field('all_domains', 'reference_many').targetEntity(domain).targetField(nga.field('name')).label('Communautés hérités'),
+        nga.field('primary_institute', 'reference').targetEntity(institute).targetField(nga.field('name')).label('Institut Janus'),
+        nga.field('additional_institutes', 'reference_many').targetEntity(institute).targetField(nga.field('name')).label('Instituts secondaire'),
+        nga.field('primary_unit', 'reference').targetEntity(unit).targetField(nga.field('name')).label('Unité Janus'),
+        nga.field('additional_units', 'reference_many').targetEntity(unit).targetField(nga.field('name')).label('Unités secondaires')
+    ])
+    .filters([
+        nga.field('match').label('Recherche global').pinned(true),
+        nga.field('like_janus_account.username').label('Login'),
+        nga.field('domain.name', 'reference')
+        .targetEntity(domain)
+        .targetField(nga.field('name'))
+        .remoteComplete(true)
+        .label('Communautés'),
+        nga.field('janus_account.primary_institute', 'reference')
+        .targetEntity(institute)
+        .targetField(nga.field('like_name').map((_, entry) => entry.name))
+        .remoteComplete(true)
+        .label('Institut Janus'),
+        nga.field('janus_account.primary_unit', 'reference')
+        .targetEntity(unit)
+        .targetField(nga.field('like_name').map((_, entry) => entry.name))
+        .remoteComplete(true)
+        .label('Unité Janus'),
+        nga.field('unit.id', 'reference')
+        .targetEntity(unit)
+        .targetField(nga.field('like_name').map((_, entry) => entry.name))
+        .remoteComplete(true)
+        .label('Unité(Nom)'),
+        nga.field('unit.id', 'reference')
+        .targetEntity(unit)
+        .targetField(nga.field('like_code').map((_, entry) => entry.code))
+        .remoteComplete(true)
+        .label('Unité(Code)'),
+        nga.field('institute.id', 'reference')
+        .targetEntity(institute)
+        .targetField(nga.field('like_name').map((_, entry) => entry.name))
+        .remoteComplete(true)
+        .label('Institut(Nom)'),
+        nga.field('institute.id', 'reference')
+        .targetEntity(institute)
+        .targetField(nga.field('like_code').map((_, entry) => entry.code))
+        .remoteComplete(true)
+        .label('Institut(Code)')
+    ])
+    .sortField('username')
+    .sortDir('DESC')
+    .exportFields([
+        janusAccount.listView().fields()
+    ])
+    .listActions(['edit', 'delete']);
+
     janusAccount.editionView()
     .title('Compte JANUS {{ entry.values.username }}')
     .fields([
@@ -73,6 +74,7 @@ export default function (nga, admin) {
         nga.field('primary_unit', 'reference').targetEntity(unit).targetField(nga.field('name')).editable(false).label('Unité Janus'),
         nga.field('additional_units', 'reference_many').targetEntity(unit).targetField(nga.field('name')).label('Unités secondaires')
     ]);
+
     janusAccount.creationView()
     .title('Compte JANUS')
     .fields([
@@ -80,7 +82,7 @@ export default function (nga, admin) {
         nga.field('domains', 'reference_many').targetEntity(admin.getEntity('domains')).targetField(nga.field('name')).label('Communautés'),
         nga.field('additional_institutes', 'reference_many').targetEntity(institute).targetField(nga.field('name')).label('Instituts secondaire'),
         nga.field('additional_units', 'reference_many').targetEntity(unit).targetField(nga.field('name')).label('Unités secondaires')
-    ])
+    ]);
 
     return janusAccount;
 }
