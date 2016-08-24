7ab7ae71b5bba1171870156929231daa81f163fc
ThieryMichel <thiery@marmelab.com>
2016-08-24 16:07:22 +0200

    add new field to janus_account

--- js/config/janusAccounts.js
+++ js/config/janusAccounts.js
@@ -10,7 +10,8 @@ export default function (nga, admin) {
     .title('Comptes JANUS')
     .perPage(20)
     .fields([
-        nga.field('janus_account.username').map((_, entry) => entry.username).isDetailLink(true).label('Username'),
+        nga.field('uid').isDetailLink(true).label('Uid'),
+        nga.field('mail').isDetailLink(true).label('Mail'),
         nga.field('domains', 'reference_many').targetEntity(domain).targetField(nga.field('name')).label('Communautés'),
         nga.field('all_domains', 'reference_many').targetEntity(domain).targetField(nga.field('name')).label('Communautés hérités'),
         nga.field('primary_institute', 'reference').targetEntity(institute).targetField(nga.field('name')).label('Institut Janus'),
@@ -20,7 +21,9 @@ export default function (nga, admin) {
     ])
     .filters([
         nga.field('match').label('Recherche global').pinned(true),
-        nga.field('like_janus_account.username').label('Login'),
+        nga.field('like_janus_account.uid').label('Uid'),
+        nga.field('like_janus_account.mail').label('Mail'),
+        nga.field('janus_account.cnrs', 'boolean').label('Cnrs'),
         nga.field('domain.name', 'reference')
         .targetEntity(domain)
         .targetField(nga.field('name'))
@@ -57,7 +60,7 @@ export default function (nga, admin) {
         .remoteComplete(true)
         .label('Institut(Code)')
     ])
-    .sortField('username')
+    .sortField('uid')
     .sortDir('DESC')
     .exportFields([
         janusAccount.listView().fields()
@@ -65,23 +68,20 @@ export default function (nga, admin) {
     .listActions(['edit', 'delete']);
 
     janusAccount.editionView()
-    .title('Compte JANUS {{ entry.values.username }}')
+    .title('Compte JANUS {{ entry.values.uid }}')
     .fields([
-        nga.field('username').label('Username'),
+        nga.field('uid').editable(false).label('Uid'),
+        nga.field('name').editable(false).label('Name'),
+        nga.field('firstname').editable(false).label('First Name'),
+        nga.field('mail').editable(false).label('Mail'),
+        nga.field('last_connexion', 'date').format('dd/MM/yyyy').editable(false).label('Last Connexion'),
+        nga.field('cnrs', 'boolean').editable(false).label('CNRS'),
         nga.field('domains', 'reference_many').targetEntity(domain).targetField(nga.field('name')).label('Communautés'),
         nga.field('primary_institute', 'reference').targetEntity(institute).targetField(nga.field('name')).editable(false).label('Institut Janus'),
         nga.field('additional_institutes', 'reference_many').targetEntity(institute).targetField(nga.field('name')).label('Instituts secondaire'),
         nga.field('primary_unit', 'reference').targetEntity(unit).targetField(nga.field('name')).editable(false).label('Unité Janus'),
-        nga.field('additional_units', 'reference_many').targetEntity(unit).targetField(nga.field('name')).label('Unités secondaires')
-    ]);
-
-    janusAccount.creationView()
-    .title('Compte JANUS')
-    .fields([
-        nga.field('username').label('Username'),
-        nga.field('domains', 'reference_many').targetEntity(admin.getEntity('domains')).targetField(nga.field('name')).label('Communautés'),
-        nga.field('additional_institutes', 'reference_many').targetEntity(institute).targetField(nga.field('name')).label('Instituts secondaire'),
-        nga.field('additional_units', 'reference_many').targetEntity(unit).targetField(nga.field('name')).label('Unités secondaires')
+        nga.field('additional_units', 'reference_many').targetEntity(unit).targetField(nga.field('name')).label('Unités secondaires'),
+        nga.field('janus_account.comment', 'text').label('Comment')
     ]);
 
     return janusAccount;
