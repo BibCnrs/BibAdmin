e53997b39efc0b8ce4cf9f2daa720d38521ecf4b
ThieryMichel <thiery@marmelab.com>
2016-08-25 17:11:05 +0200

    inline directive template

--- js/directives/bibPassword.js
+++ js/directives/bibPassword.js
@@ -1,7 +1,26 @@
 const bibPassword = (generatePassword) => {
     return {
         restrict: 'E',
-        templateUrl: '/js/directives/templates/bibPassword.html',
+        template: (
+`<div class="input-group">
+    <input
+        id="{{ field.name() }}"
+        name="{{ field.name() }}"
+        class="{{ field.getCssClasses() }} form-control"
+        placeholder="{{field.validation().minlength}} characters minimum"
+        type="text"
+        ng-model="entry.values[field.name()]"
+        ng-required="field.validation().required"
+        maxlength="{{ field.validation().maxlength }}"
+        onClick="select()"
+    >
+    <span class="input-group-btn">
+        <button class="btn btn-default" ng-click="generatePassword($event)">
+            Generate
+        </button>
+    </span>
+</div>`
+        ),
         link: function ($scope, $element) {
             $scope.generatePassword = function ($event) {
                 $event.preventDefault();
