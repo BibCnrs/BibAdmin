3619f860b71a85234f9cedec2182d7e7499fb670
ThieryMichel <thiery@marmelab.com>
2017-01-06 16:14:00 +0100

    better style for bib-image

--- js/directives/bibImage.js
+++ js/directives/bibImage.js
@@ -2,24 +2,32 @@ const bibImage = (readImageAsDataUrl) => {
     return {
         restrict: 'E',
         template: (
-`<div class="image-input">
-    <div class="">
-        <input
-            type="file"
-            accept="image/*"
-        />
+`<div class="row">
+    <div class="col-md-2">
+        <a class="btn btn-default" ng-click="selectFile()">
+            <span translate="BROWSE"></span>
+        </a>
     </div>
-    <div ng-if="entry.values[field.name()]">
+    <div class="col-md-2" ng-if="entry.values[field.name()]">
         <img src="{{entry.values[field.name()]}}" />
     </div>
+    <input
+        type="file"
+        accept="image/*"
+        style="display:none"
+    />
 </div>`
         ),
-        link: function ($scope, $element) {
-            $element.bind('change', function (changeEvent) {
+        link: function (scope, element) {
+            const input = element.find('input')[0];
+            scope.selectFile = function () {
+                input.click();
+            };
+            element.bind('change', function (changeEvent) {
                 readImageAsDataUrl(changeEvent.target.files[0])
                 .then(result => {
-                    $scope.entry.values[$scope.field.name()] = result;
-                    $scope.$digest();
+                    scope.entry.values[scope.field.name()] = result;
+                    scope.$digest();
                 });
             });
         }
