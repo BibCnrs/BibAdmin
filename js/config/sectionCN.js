df22150da1c4fadff56c81f8f1f6267cbd4e73a5
ThieryMichel <thiery@marmelab.com>
2017-01-12 15:48:28 +0100

    add filter

--- js/config/sectionCN.js
+++ js/config/sectionCN.js
@@ -5,12 +5,27 @@ export default function (nga, admin) {
     const institute = admin.getEntity('institutes');
 
     database.listView()
-    .actions(['create', 'batch'])
+    .actions(['filter', 'create', 'batch'])
     .title('Sections du comité national')
     .perPage(20)
     .fields([
-        nga.field('name').isDetailLink(true).label('Nom'),
-        nga.field('code').isDetailLink(true).label('Nom'),
+        nga.field('section_cn.name').map((_, entry) => entry.name).isDetailLink(true).label('Nom'),
+        nga.field('section_cn.code').map((_, entry) => entry.code).isDetailLink(true).label('Code'),
+    ])
+    .filters([
+        nga.field('match').label('Recherche globale').pinned(true),
+        nga.field('like_section_cn.name').label('Nom'),
+        nga.field('like_section_cn.code').label('Code'),
+        nga.field('primary_institute.institute_id', 'reference')
+        .targetEntity(institute)
+        .targetField(nga.field('name'))
+        .remoteComplete(true)
+        .label('Instituts principaux'),
+        nga.field('secondary_institute.institute_id', 'reference')
+        .targetEntity(institute)
+        .targetField(nga.field('name'))
+        .remoteComplete(true)
+        .label('Instituts secondaires'),
     ])
     .sortField('name')
     .sortDir('DESC')
@@ -27,7 +42,7 @@ export default function (nga, admin) {
     ]);
 
     database.creationView()
-    .title('Nouvel Sections du comité national')
+    .title('Nouvelle Sections du comité national')
     .fields([
         nga.field('name').label('Nom'),
         nga.field('code').label('Code'),
