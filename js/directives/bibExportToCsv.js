c3169033f870d4a7633656c6e23a744ce7319ec5
djhi <gildas.garcia@gmail.com>
2017-01-17 09:27:22 +0100

    [RFR] Add spinner to export button

--- js/directives/bibExportToCsv.js
+++ js/directives/bibExportToCsv.js
@@ -10,6 +10,7 @@ export default function maExportToCsvButton ($stateParams, Papa, notification, A
         },
         link: (scope) => {
             scope.label = scope.label || 'EXPORT';
+            scope.downloading = false;
             scope.datastore = scope.datastore();
             scope.entity = scope.entity();
             const exportView = scope.entity.exportView();
@@ -27,6 +28,7 @@ export default function maExportToCsvButton ($stateParams, Papa, notification, A
 
             scope.exportToCsv = function () {
                 let rawEntries;
+                scope.downloading = true;
 
                 ReadQueries.getAll(exportView, -1, $stateParams.search, $stateParams.sortField, $stateParams.sortDir)
                     .then(response => {
@@ -44,6 +46,7 @@ export default function maExportToCsvButton ($stateParams, Papa, notification, A
 
                         if (window.navigator && window.navigator.msSaveOrOpenBlob) { // Manage IE11+ & Edge
                             const blob = new Blob([csv], { type: 'text/csv' });
+                            scope.downloading = false;
                             return window.navigator.msSaveOrOpenBlob(blob, blobName);
                         }
 
@@ -56,6 +59,7 @@ export default function maExportToCsvButton ($stateParams, Papa, notification, A
                             const url = windowUrl.createObjectURL(blob);
                             fakeLink.setAttribute('href', url);
                             fakeLink.setAttribute('download', blobName);
+                            scope.downloading = false;
                             fakeLink.click();
                             windowUrl.revokeObjectURL(url);
                             return;
@@ -63,16 +67,20 @@ export default function maExportToCsvButton ($stateParams, Papa, notification, A
 
                         fakeLink.setAttribute('href', 'data:application/octet-stream;charset=utf-8,' + encodeURIComponent(csv));
                         fakeLink.setAttribute('download', blobName);
+                        scope.downloading = false;
                         fakeLink.click();
                     }, function (error) {
                         notification.log(error.message, {addnCls: 'humane-flatty-error'});
+                        scope.downloading = false;
                     });
             };
         },
         template:
 `<span ng-if="has_export">
     <a class="btn btn-default" ng-click="exportToCsv()">
-        <span class="glyphicon glyphicon-download" aria-hidden="true"></span>&nbsp;<span class="hidden-xs" translate="{{ ::label }}"></span>
+        <span ng-if="!downloading" class="glyphicon glyphicon-download" aria-hidden="true"></span>
+        <span ng-if="downloading" class="glyphicon glyphicon-repeat normal-right-spinner" aria-hidden="true"></span>
+        &nbsp;<span class="hidden-xs" translate="{{ ::label }}"></span>
     </a>
 </span>`
     };
