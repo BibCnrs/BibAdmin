2e7fc575231dec0ec5db1bb31d61ee2b28170f43
ThieryMichel <thiery@marmelab.com>
2018-03-14 15:13:55 +0100

    display link preview in revue edition

--- js/directives/index.js
+++ js/directives/index.js
@@ -1,9 +1,11 @@
 import bibPassword from './bibPassword';
 import bibImage from './bibImage';
 import bibExportToCsv from './bibExportToCsv';
+import bibRevueLinks from './bibRevueLinks';
 
 export default function (app) {
     app.directive('bibPassword', bibPassword);
     app.directive('bibImage', bibImage);
     app.directive('bibExportToCsv', bibExportToCsv);
+    app.directive('bibRevueLinks', bibRevueLinks);
 }
