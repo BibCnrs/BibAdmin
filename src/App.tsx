2e47b8c3ec5df375a6d1c05253c280d2aa021718
thibault-barrat <contact@thibault-barrat.com>
2022-10-11 13:53:27 +0200

    Feat: add french translations

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import { Admin, ListGuesser, Resource } from "react-admin";
+import polyglotI18nProvider from "ra-i18n-polyglot";
 import adminUsers from "./adminUsers";
 import dataProvider from "./dataProvider";
 import authProvider from "./authProvider";
+import langFr from "./i18n/fr";
+
+const i18nProvider = polyglotI18nProvider(() => langFr, "fr", {
+  allowMissing: true,
+});
 
 const App = () => (
-  <Admin dataProvider={dataProvider} authProvider={authProvider}>
+  <Admin
+    i18nProvider={i18nProvider}
+    dataProvider={dataProvider}
+    authProvider={authProvider}
+    requireAuth
+  >
     <Resource name="adminUsers" {...adminUsers} />
   </Admin>
 );
