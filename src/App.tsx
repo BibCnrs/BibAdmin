6dbd66e454b9296fdbf42f09060d13ae7d367ed5
thibault-barrat <contact@thibault-barrat.com>
2022-10-11 11:07:19 +0200

    Feat: add authProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
-import { Admin } from "react-admin";
+import { Admin, ListGuesser, Resource } from "react-admin";
 import dataProvider from "./dataProvider";
+import authProvider from "./authProvider";
 
-const App = () => <Admin dataProvider={dataProvider} />;
+const App = () => (
+  <Admin dataProvider={dataProvider} authProvider={authProvider}>
+    <Resource name="adminUsers" list={ListGuesser} />
+  </Admin>
+);
 
 export default App;
