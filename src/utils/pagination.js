c59fea04920df988806d6ba121f591f730c50d3a
thibault-barrat <contact@thibault-barrat.com>
2022-10-11 10:09:55 +0200

    Feat: init new react admin

--- src/utils/pagination.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Pagination } from "react-admin";
-
-export const PostPagination = props => (
-  <Pagination rowsPerPageOptions={[5, 10, 25, 50]} {...props} />
-);
