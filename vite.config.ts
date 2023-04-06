ba8c8af8a0fa9a2e9e96c3a7eb262ea9cdca6ff5
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2023-04-06 14:59:41 +0200

    build: update chunks system

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,6 @@ import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 
 const regex = /(.*node_modules\/)([^\/]+)(.*)/;
-function hash(s) {
-    let h = 0;
-    for(let i = 0; i < s.length; i++)
-        h = Math.imul(31, h) + s.charCodeAt(i) | 0;
-    let hash = h.toString(16).replace('-', '');
-    if (hash.length === 8) return hash;
-    if (hash.length > 8) return hash.slice(0, 9);
-    while (hash.length < 8) {
-        h = Math.imul(31, h);
-        hash = h.toString(16).replace('-', '');
-    }
-    if (hash.length > 8) return hash.slice(0, 9);
-    return h.toString(16).replace('-', '');
-}
-
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), eslint()],
     base: '/admin/',
@@ -34,7 +18,7 @@ export default defineConfig({
                 manualChunks: (id, meta) => {
                     if (id.includes('node_modules')) {
                         const name = id.match(regex)[2].replace('@', '');
-                        if (meta.getModuleInfo(id).isIncluded) return hash(name);
+                        if (meta.getModuleInfo(id).isIncluded) return name;
                     }
                 },
             },
